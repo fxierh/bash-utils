5294d77efb75efd4f6fa0aa90ffa346c01661a49
Feilian Xie <fxie@redhat.com>
2024-04-22 03:35:56 +0800

    Remove golang.sh which lacks broad utility for users.
    Signed-off-by: Feilian Xie <fxie@redhat.com>

--- golang.sh
+++
@@ -1,8 +0,0 @@
-export GOROOT=/usr/local/go
-export GOPATH=/Users/fxie/go
-export GOBIN=/Users/fxie/go/bin
-export PATH=$PATH:/Users/fxie/go/bin
-function usego1.20() {
-    export GOROOT=/Users/fxie/go1.20.14
-    export PATH=$GOROOT/bin:$PATH
-}
