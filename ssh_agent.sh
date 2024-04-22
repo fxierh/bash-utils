0b736dadabe2920d448779d79b231b231893df74
Feilian Xie <fxie@redhat.com>
2024-04-22 15:12:56 +0800

    Make ssh-agent initializations a function
    Signed-off-by: Feilian Xie <fxie@redhat.com>

--- ssh_agent.sh
+++ ssh_agent.sh
@@ -1,11 +1,9 @@
 # shellcheck disable=SC2154
-if [[ "$enabled_ssh_agent" != true ]]; then
-    return 0
-fi
-
-eval "$(ssh-agent -s)"
-while read -r -d '' file; do
-    if grep -q "PRIVATE KEY" "$file"; then
-        ssh-add "$file"
-    fi
-done < <(find "$default_ssh_dir" -type f \! -name "*.pub" \! -name "known_hosts*" -print0)
+function ssh_agent_init() {
+    eval "$(ssh-agent -s)"
+    while read -r -d '' file; do
+        if grep -q "PRIVATE KEY" "$file"; then
+            ssh-add "$file"
+        fi
+    done < <(find "$default_ssh_dir" -type f \! -name "*.pub" \! -name "known_hosts*" -print0)
+}
