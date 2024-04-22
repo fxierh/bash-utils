f1e7ef8e4fa6a0cc5efce7ef83e9394a738c3860
Fan Fan <ffan@redhat.com>
2024-04-22 23:11:31 +0800

    Add fssh function.

--- ssh_agent.sh
+++ ssh_agent.sh
@@ -7,3 +7,22 @@ function ssh_agent_init() {
         fi
     done < <(find "$default_ssh_dir" -type f \! -name "*.pub" \! -name "known_hosts*" -print0)
 }
+
+
+function fssh()
+{
+
+# Story:Beaker physical machine alway have same hostname even it got OS re-installed.
+#       When you ssh the sanme machine with another os, it will said: different  fingerprint.
+#       This function is to remote the fingerprint in the ~/.ssh/know_hosts.
+#
+# Useage: fssh root@<remote_host_name>
+
+    remote_hostname=$(echo $1 | awk -F "@" '{print $2}')
+    if (grep "$remote_hostname" ~/.ssh/known_hosts); then
+        sed -i "/^$remote_hostname/d" ~/.ssh/known_hosts
+    fi
+
+    ssh $1
+
+}
