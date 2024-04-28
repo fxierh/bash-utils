2a4dd9ffd1feecd69cc001b890b95604c2265b80
Feilian Xie <fxie@redhat.com>
2024-04-28 23:45:30 +0800

    Move notify and beep to framework.sh
    Signed-off-by: Feilian Xie <fxie@redhat.com>

--- framework.sh
+++ framework.sh
@@ -1,4 +1,5 @@
 # Functions utilized by other utilities and directly by end users
+# shellcheck disable=SC2034
 
 function hle() {
     # Check if tput is available and supports at least 8 colors
@@ -18,4 +19,38 @@ function hle() {
 # https://en.wikipedia.org/wiki/ANSI_escape_code#OSC
 function wint() {
     printf '\033]0;%s\007' "$1"
+}
+
+function beep() {
+    echo -ne "\a"
+}
+
+function notify() {
+    local num_notifications=3
+
+    # Parse options
+    local OPTIND
+    while getopts "n:" opt; do
+        case $opt in
+        n)
+            num_notifications=$OPTARG
+            ;;
+        \?)
+            return 1
+            ;;
+        esac
+    done
+    shift $((OPTIND - 1))
+
+    # Execute the command
+    "$@"
+    local exit_status=$?
+
+    # Make a number of noises
+    for i in $(seq 1 "$num_notifications"); do
+        beep
+    done
+
+    # Return the status of the command
+    return $exit_status
 }
