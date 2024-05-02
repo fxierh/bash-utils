e769f459ef6119b1a298da86b162796a2bcdcdc0
Feilian Xie <fxie@redhat.com>
2024-05-03 04:14:28 +0800

    Enhance list-utils: support -a (include internal functions) and -f (print filename)
    Signed-off-by: Feilian Xie <fxie@redhat.com>

--- hack/list-utils.sh
+++ hack/list-utils.sh
@@ -2,6 +2,14 @@
 
 set -euo pipefail
 
+function usage() {
+    echo "Usage: ${0##*/} [-a] [-f] directory
+
+Options:
+-a: include internal functions (those starting with an underscore)
+-f: print the filename before listing its functions"
+}
+
 function extract_function_names_from_file() {
     awk '
     /^[[:blank:]]*function[[:blank:]]+/ { sub(/\(\)/, "", $2); print $2 }
@@ -10,13 +18,54 @@ function extract_function_names_from_file() {
 }
 
 function filter_internal_functions() {
-    grep -v '^_'
+    grep -v '^_' <<< "$1"
 }
 
 function get_function_names() {
-    while read -r -d '' file; do \
-        extract_function_names_from_file "$file" | filter_internal_functions || :
-    done < <(find "$1" -depth 1 -name '*.sh' -print0)
+    local directory="$1"
+    local file
+    local functions_names
+
+    while read -r -d '' file; do
+        # Print file name if requested
+        if [[ "$print_file_names" == true ]]; then
+            echo ">>>>> $(basename "$file")"
+        fi
+
+        # Get function names
+        functions_names="$(extract_function_names_from_file "$file")"
+        # Filter out internal functions unless requested otherwise
+        if [[ "$include_internal_functions" != true ]]; then
+            functions_names=$(filter_internal_functions "$functions_names")
+        fi
+
+        # Print function names
+        if [[ -n "$functions_names" ]]; then
+            echo "$functions_names"
+        fi
+    done < <(find "$directory" -depth 1 -name '*.sh' -print0)
 }
 
+# Default values
+include_internal_functions=false
+print_file_names=false
+
+# Parse options
+while getopts "af" opt; do
+    case $opt in
+    a)
+        include_internal_functions=true
+        ;;
+    f)
+        print_file_names=true
+        ;;
+    \?)
+        usage
+        exit 1
+        ;;
+    esac
+done
+shift $((OPTIND - 1))
+
+# Main
 get_function_names "$1"
